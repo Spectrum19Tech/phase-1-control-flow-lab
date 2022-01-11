e4d0a7d2a4c8812420215d13dc1a44fded6b74d0
Jonathan Bryant <spectrum19tech@gmail.com>
2022-01-10 21:36:37 -0800

    commit tree fa90dd11933bf91167d7a7df30d7b93ae85a61fd

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(num){
   // Write your code here!
+  if(num > 2500) {
+    return 'No can do.';
+  } else if(num > 2000) {
+    return 'I will gladly take your thirty bucks.';
+  } else if(num <= 400) {
+    return 'This one is on me!';
+  };
+};
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  return (city === 'NYC') ? 'Ok, sounds good.' : 'No go.';
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    case 'thanks for everything':
+      return 'Bye.'
+  };
 }
